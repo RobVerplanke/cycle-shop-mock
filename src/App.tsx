cc035b544b39c0db7d1ee943dfd4c7f52cd84cca
Rob <rverplanke@gmail.com>
2025-04-26 19:12:29 +0200

    style: setup responsive navbar layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import './scss/style.scss';
 
 function App() {
   return (
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,55 @@
 import { Link } from 'react-router-dom';
+import { MdPedalBike } from 'react-icons/md';
+import { IoMenu } from 'react-icons/io5';
+import { IoClose } from 'react-icons/io5';
+import { IoCart } from 'react-icons/io5';
+import { useState } from 'react';
 
 function Navbar() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const [isNavVisible, setIsNavVisible] = useState(true);
+
+  // Toggle menu hamburger icon, make menu visible if screen width becomes larger
+  function toggleMenu() {
+    setIsNavOpen(!isNavOpen);
+    setIsNavVisible(!isNavVisible);
+  }
+
   return (
-    <nav>
-      <p>logo</p>
-      <ul>
-        <li>
-          <Link to={'/'}>Home</Link>
-        </li>
-        <li>
-          <Link to={'/products'}>Shop</Link>
-        </li>
-        <li>
-          <Link to={'/about'}>About us</Link>
-        </li>
-        <li>
-          <Link to={'/contact'}>Contact</Link>
-        </li>
-        <li>
-          <Link to={'/cart'}>Cart</Link>
-        </li>
-      </ul>
+    <nav className="navbar">
+      <div className="navbar__container">
+        <div className="navbar__logo">
+          <MdPedalBike size={70} />
+        </div>
+
+        <div className="navbar__menu-icon" onClick={toggleMenu}>
+          {isNavOpen ? <IoClose size={40} /> : <IoMenu size={40} />}
+        </div>
+
+        <ul className={isNavVisible ? 'navbar__menu' : 'navbar__menu--open'}>
+          <li className="navbar__item">
+            <Link to={'/'}>HOME</Link>
+          </li>
+          <li className="navbar__item">
+            <Link to={'/products'}>BYCICLES</Link>
+          </li>
+          <li className="navbar__item">
+            <Link to={'/products'}>ACCESOIRES</Link>
+          </li>
+          <li className="navbar__item">
+            <Link to={'/about'}>ABOUT US</Link>
+          </li>
+          <li className="navbar__item">
+            <Link to={'/contact'}>CONTACT</Link>
+          </li>
+        </ul>
+
+        <div className="navbar__cart-icon">
+          <Link to={'/cart'}>
+            <IoCart size={20} />
+          </Link>
+        </div>
+      </div>
     </nav>
   );
 }
