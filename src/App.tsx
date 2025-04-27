884dcba6eca5b478c14ba4c3af54188f93c4c091
Rob <rverplanke@gmail.com>
2025-04-27 19:53:03 +0200

    refactor: implement background image
    Move navigation in to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from 'react-router-dom';
 import Footer from './components/Footer';
-import Navbar from './components/Navbar';
 import './scss/style.scss';
 
 function App() {
   return (
     <div>
-      <Navbar />
       <Outlet />
       <Footer />
     </div>
--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -1,50 +1,56 @@
 import { SiTarget } from 'react-icons/si';
+import Navbar from '../Navbar';
 
 function Header() {
   return (
     <header className="header">
-      <div className="header__title">
-        <h2>Newly Launched</h2>
+      <div className="header__navbar">
+        <Navbar />
       </div>
-      <div className="header__subtitle">
-        <h1>Kryo X26 MTB</h1>
-      </div>
-      <div className="header__specs">
-        <div className="header__specs__title">
-          <h2>Specifications:</h2>
+      <div className="header__container">
+        <div className="header__title">
+          <h2>Newly Launched</h2>
         </div>
-        <div className="header__specs">
-          <ul className="header__list">
-            <li className="header__list-item">
-              <div>
-                <SiTarget />
-              </div>
-              <div className="header__list-item__content">
-                Lightweight 18" Frame
-              </div>
-            </li>
-            <li className="header__list-item">
-              <div>
-                <SiTarget />
-              </div>
-              <div className="header__list-item__content">
-                Steel Suspension Fork
-              </div>
-            </li>
-            <li className="header__list-item">
-              <div>
-                <SiTarget />
-              </div>
-              <div className="header__list-item__content">
-                Steel Hardtail Frame
-              </div>
-            </li>
-          </ul>
+        <div className="header__subtitle">
+          <h1>Kryo X26 MTB</h1>
         </div>
-      </div>
+        <div className="header__specs-container">
+          <div className="header__specs__title">
+            <h2>Specifications:</h2>
+          </div>
+          <div className="header__specs">
+            <ul className="header__list">
+              <li className="header__list-item">
+                <div>
+                  <SiTarget />
+                </div>
+                <div className="header__list-item__content">
+                  Lightweight 18" Frame
+                </div>
+              </li>
+              <li className="header__list-item">
+                <div>
+                  <SiTarget />
+                </div>
+                <div className="header__list-item__content">
+                  Steel Suspension Fork
+                </div>
+              </li>
+              <li className="header__list-item">
+                <div>
+                  <SiTarget />
+                </div>
+                <div className="header__list-item__content">
+                  Steel Hardtail Frame
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
 
-      <div className="header__buy-button">
-        <button>BUY NOW</button>
+        <div className="header__buy-button">
+          <button>BUY NOW</button>
+        </div>
       </div>
     </header>
   );
